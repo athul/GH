49b0b3997b828c0f85fedc73ce5dce3fdd2339cf
athul <athul8720@gmail.com>
2020-08-25 09:54:52 +0530

    New Themes and images

---
+++ tilda.js
@@ -0,0 +1,43 @@
+const blue = '#0af'
+
+export default {
+    googleFont:'https://fonts.googleapis.com/css2?family=Montserrat:wght@600',
+    fonts: {
+    heading: '"Avenir Next", system-ui, sans-serif',
+    monospace:'"Cascadia Code",system-ui,monospace',
+    body:'"Product Sans",system-ui',
+  },
+  colors: {
+    text: '#fff',
+    background: '#111',
+    primary: 'cyan',
+    black: '#000',
+  },
+  fontWeights: {
+    heading: 600,
+    bold: 600,
+  },
+  text: {
+    heading: {
+      padding:'10px',
+      fontFamily:'heading',
+      textTransform: 'uppercase',
+      letterSpacing: '0.1em',
+    },
+  },
+  styles: {
+    pre: {
+      color: 'primary',
+      bg: 'black',
+    },
+    code: {
+      color: 'primary',
+    },
+    p:{
+        margin:'28px',
+    },
+    li:{
+        marginLeft:'40px'
+    }
+  },
+}
