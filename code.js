89cd30f868ad267b85a3ab14b9432a268f9ecdac
Athul Cyriac Ajay <athul8720@gmail.com>
2020-09-01 15:28:57 +0530

    MBCET

--- code.js
+++ code.js
@@ -3,34 +3,7 @@ import SyntaxHighlighter from 'react-syntax-highlighter'
 import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 
-const Provider = props => (
-  <div>
-    {props.children}
-    <div
-      css={{
-        position: 'fixed',
-        right: 0,
-        //left:1,
-        bottom: -1,
-        margin: 10,
-      }}
-    >
-      <p style={{fontSize:24,fontFamily:`Jetbrains Mono,monospace`,color:`yellow`}}>@athulcajay</p>
-    </div>
-    <div
-      css={{
-        position: 'fixed',
-        //right: 0,
-        left:1,
-        bottom: -1,
-        margin: 10,
-      }}
-    >
-      <p style={{fontSize:24,fontFamily:`Jetbrains Mono,monospace`,color:`lime`}}>@athul</p>
-      
-    </div>
-  </div>
-)
+
 export const getLanguage = className => {
   const match = /language-(\w*)/.exec(className || 'language-javascript')
   let lang = 'javascript'
@@ -51,6 +24,5 @@ export default {
   components: {
     pre,
     code,
-    Provider,
   },
 }
---
+++ pro.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Provider = props => (
+    <div>
+      {props.children}
+      <div
+        css={{
+          position: 'fixed',
+          right: 0,
+          //left:1,
+          bottom: -1,
+          margin: 10,
+        }}
+      >
+        <p style={{fontSize:24,fontFamily:`Jetbrains Mono,monospace`,color:`yellow`}}>@athulcajay</p>
+      </div>
+      <div
+        css={{
+          position: 'fixed',
+          //right: 0,
+          left:1,
+          bottom: -1,
+          margin: 10,
+        }}
+      >
+        <p style={{fontSize:24,fontFamily:`Jetbrains Mono,monospace`,color:`lime`}}>@athul</p>
+        
+      </div>
+    </div>
+  )
+  export default {
+    components: {
+      Provider
+    },
+  }
